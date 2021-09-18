72d2e33e27c2e51ff3edc1c16a9b59d176509a73
forshaw <sailonbr@gmail.com>
2021-09-18 18:47:21 -0300

    Emulando DHT22 REAL

--- src/main.c
+++ src/main.c
@@ -11,18 +11,44 @@ int main(void) {
     // Relembrando: DHT22 usa PIN6, PD6.
     uint8_t I_RH, D_RH, I_Temp, D_Temp, CheckSum;
 
+    // Para printar no USART:
+    uint8_t i = 0;
+    unsigned char data[] = "Carlos e lindo";
+    unsigned char temp[] = "Temperatura: ";
+    unsigned char hum[] = "Umidade: ";
+
+
 
     while (1) {
+        // Pedir data:
         Request(); /* send start pulse */
         Response(); /* receive response */
+
+        // Receber data:
         I_RH = Receive_data(); /* store first eight bit in I_RH */
         D_RH = Receive_data(); /* store next eight bit in D_RH */
+        while (data[i] != 0) /* print temp */ {
+            USART_Transmit(hum[i]);
+            i++;
+        }
+        USART_Transmit(I_RH);
+        USART_Transmit(D_RH);
+
         I_Temp = Receive_data(); /* store next eight bit in I_Temp */
         D_Temp = Receive_data(); /* store next eight bit in D_Temp */
+        while (data[i] != 0) /* print temp */ {
+            USART_Transmit(temp[i]);
+            i++;
+        }
+        USART_Transmit(I_Temp);
+        USART_Transmit(I_Temp);
+
+
         CheckSum = Receive_data(); /* store next eight bit in CheckSum */
 
 
-        USART_Transmit(I_RH);
+
+
     }
 
     return 0;
